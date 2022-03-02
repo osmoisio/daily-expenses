b5292cbb86581f0bd33b9edc02cd043c2a675b14
Ossi Moisio <osmoisio@gmail.com>
2022-03-02 15:45:11 +0200

    Formatting fixed and README.md added

---
+++ daily_expenses/README.md
@@ -0,0 +1,6 @@
+Program to write down daily expenses
+
+Planned features:
+- Save to file and read from file
+- Count total in each day in each category and draw histogram
+- GUI
--- daily_expenses/src/daily_expenses/Expense.java
+++ daily_expenses/src/daily_expenses/Expense.java
@@ -1,44 +1,44 @@
 package daily_expenses;
 
 public class Expense {
-	private String category;
-	private double sum;
-	private String description;
-	
-	public Expense(String category, double sum, String description) {
-		this.category = category;
-		this.sum = sum;
-		this.description = description;
-	}
-	
-	public Expense() {
-	}
-	
-	public String toString() {
-		return category + ", " + sum + ", " + description;
-	}
+  private String category;
+  private double sum;
+  private String description;
 
-	public String getCategory() {
-		return category;
-	}
+  public Expense(String category, double sum, String description) {
+    this.category = category;
+    this.sum = sum;
+    this.description = description;
+  }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+  public Expense() {
+  }
 
-	public double getSum() {
-		return sum;
-	}
+  public String toString() {
+    return category + ", " + sum + ", " + description;
+  }
 
-	public void setSum(double sum) {
-		this.sum = sum;
-	}
+  public String getCategory() {
+    return category;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public void setCategory(String category) {
+    this.category = category;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public double getSum() {
+    return sum;
+  }
+
+  public void setSum(double sum) {
+    this.sum = sum;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
